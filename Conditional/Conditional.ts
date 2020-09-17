58200540146398d5e550751b5bf41a13c0888e33
saeid nasiri <name62777@gmail.com>
2020-09-17 13:19:05 +0430

    feat/Add Conditional types

---
+++ Conditional/Conditional.ts
@@ -0,0 +1,22 @@
+type TypeName<T> = T extends string
+  ? "string"
+  : T extends number
+  ? "number"
+  : T extends boolean
+  ? "boolean"
+  : T extends undefined
+  ? "undefined"
+  : T extends Function
+  ? "function"
+  : "object";
+
+type T0 = TypeName<string>;
+//   ^ = type T0 = "string"
+type T1 = TypeName<"a">;
+//   ^ = type T1 = "string"
+type T2 = TypeName<true>;
+//   ^ = type T2 = "boolean"
+type T3 = TypeName<() => void>;
+//   ^ = type T3 = "function"
+type T4 = TypeName<string[]>;
+//   ^ = type T4 = "object"
